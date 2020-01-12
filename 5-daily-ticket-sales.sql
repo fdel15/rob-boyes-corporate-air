4b1f4c67fca43630a08f4669e67c3484f2723299
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-12 18:05:17 -0500

    Create 5-daily-ticket-sales.sql

---
+++ 5-daily-ticket-sales.sql
@@ -0,0 +1,32 @@
+IF OBJECT_ID('dbo.v_daily_ticket_sales') is NOT NULL
+	DROP VIEW dbo.v_daily_ticket_sales;
+GO
+
+CREATE VIEW dbo.v_daily_ticket_sales AS
+
+SELECT  TOP 10000
+        cast(getdate() as date) as Report_Run_Date,
+        td.PrimaryDocNbr as PrimaryDocNbr,
+        td.PnrLocatorId as PnrLocatorId,
+        td.PnrCreateDate as PnrCreateDate,
+        tx.TaxCode as TaxCode,
+        isnull(td.PassengerType, 'ADT') as Pax_Type,
+        cast(td.TotalDocAmt as decimal(8,2)) as TotalDocAmt,
+        cast (SUM(tx.TaxAmt) as decimal(8,2)) as Total_Tax
+
+FROM    dbo.tktDocument td
+        LEFT JOIN dbo.tktTax tx on td.PrimaryDocNbr = tx.PrimaryDocNbr 
+                                and td.PnrLocatorId = tx.PnrLocatorId
+                                and td.PnrCreateDate = tx.PnrCreateDate
+
+WHERE   td.SourceSystemId = 'FC'
+        and td.VcrCreateDate > cast(getdate() - 1 as date) 
+        
+GROUP BY  td.PrimaryDocNbr,
+          td.PnrLocatorId,
+          td.PnrCreateDate,
+          tx.TaxCode,
+          td.PassengerType,td.TotalDocAmt
+
+ORDER BY  td.PnrLocatorId, td.PrimaryDocNbr
+GO
