a86474533a1d0c43b4d3de39cc5c8013aad14a79
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-12 13:12:44 -0500

    Create 3-daily-partner-sales.sql

---
+++ 3-daily-partner-sales.sql
@@ -0,0 +1,14 @@
+IF OBJECT_ID('dbo.v_daily_partner_sales') is NOT NULL
+	DROP VIEW dbo.v_daily_partner_sales;
+GO
+
+CREATE VIEW dbo.daily_partner_sales AS
+
+  SELECT  TOP 10000
+          *
+
+  FROM    dbo.v_daily_partner_sales_base
+      
+  ORDER BY 2,3
+GO
+
