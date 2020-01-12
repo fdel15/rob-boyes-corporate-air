05b46c56f55678f3459b6967ce482599491ccf50
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-12 10:27:57 -0500

    Update and rename sales-and-date-view.sql to 1-sales-and-date-view.sql

---
+++ 1-sales-and-date-view.sql
@@ -0,0 +1,24 @@
+IF OBJECT_ID('dbo.sales_and_date') is NOT NULL
+	DROP VIEW dbo.sales_and_date;
+GO
+
+CREATE VIEW dbo.sales_and_date AS
+
+	SELECT	TOP 10000
+			tc.PNRLocatorId,
+			tc.ServiceStartDate,
+			tc.MarketingFlightNbr,
+			tc.ServiceStartCity,
+			tc.ServiceEndCity,
+			td.CustomerFullName as pax_name,
+			td.TotalDocAmt,
+			tc.CouponStatus
+
+	FROM	dbo.tktcoupon tc
+			inner join dbo.tktdocument td on td.primarydocnbr = tc.primarydocnbr
+
+	WHERE	td.SourceSystemId = 'fc'
+
+	ORDER BY tc.servicestartdate, tc.servicestartcity, tc.pnrlocatorid
+GO
+
