4e03f3c8ce731de1dc5af8653334bb568b32e706
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-21 11:39:52 -0500

    Update 8-expiring-tickets.sql

--- views/8-expiring-tickets.sql
+++ views/8-expiring-tickets.sql
@@ -5,24 +5,11 @@ GO
 CREATE VIEW dbo.v_expiring_tickets AS
 
 
-SELECT  TOP 200000
-		tc.vcrcreatedate as Original_Ticketing_Date,
-        cast(cast(tc.vcrcreatedate as datetime) + 365 as date) as Ticket_Expiry_Date,
-        tc.couponseqnbr as Coupon_Seq_Nbr,
-        tc.Servicestartcity as Current_Origin,
-        tc.serviceendcity as Current_Destination,
-        tc.pnrlocatorid as Booking_Locator,
-        td.customerfullname as Pax_Name
+	SELECT  TOP 200000
+			*
+	
+	FROM	v_expiring_tickets_base
 
-FROM    dbo.tktcoupon tc
-        inner join dbo.tktDocument td on tc.primarydocnbr = td.primarydocnbr
-          and tc.pnrlocatorid = td.pnrlocatorid
-          and td.pnrcreatedate = tc.pnrcreatedate
+	ORDER BY Current_Origin, Ticket_Expiry_Date, Current_Destination
 
-WHERE   cast(cast(tc.vcrcreatedate as datetime) + 365 as date) < cast(getdate() + 7 as date) 
-        and cast(cast(tc.vcrcreatedate as datetime) + 365 as date)  > cast(getdate() as date)
-        and td.sourcesystemid = 'fc'
-        and tc.couponstatus = 'ok'
-
-ORDER BY 4,2,5
 GO
