e935b687fd0c25eae91169a21f4e1d8a8cb27919
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-21 11:31:59 -0500

    Update 7a-daily-summary-by-route-base.sql

--- views/7a-daily-summary-by-route-base.sql
+++ views/7a-daily-summary-by-route-base.sql
@@ -5,7 +5,7 @@ GO
 CREATE VIEW dbo.v_daily_summary_by_route_base AS
 
 SELECT  DISTINCT
-        cast(getdate() as date) as Report_Run_Date,
+        getdate() as Report_Run_Date,
         tc.ServiceStartCity as Origin,
         tc.ServiceEndCity as Destination,
         count(rf.PnrLocatorId) as Number_of_Bookings,
@@ -20,7 +20,7 @@ FROM    dbo.tktDocument td
                                     and rf.pnrcreatedate = tc.pnrcreatedate
                                     and INTRAPNRSETNBR = 0
 where   td.sourcesystemid = 'fc'
-        and td.PNRCREATEDATE > cast(getdate() - 10 as date) 
+        --and td.PNRCREATEDATE > cast(getdate() - 10 as date) 
         
 group by tc.ServiceStartCity, tc.ServiceEndCity
 GO
