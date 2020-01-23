bb6f3885a4a1b7664eec154da4f1569e5b18304e
Frank DelPidio <frankdelpidio@gmail.com>
2020-01-23 01:49:08 -0500

    Update 2-passengers-and-date-view.sql

--- views/2-passengers-and-date-view.sql
+++ views/2-passengers-and-date-view.sql
@@ -13,11 +13,13 @@ SELECT	top 200000
 
 FROM	dbo.ACSPaxFlight
 
-WHERE	dbo.ACSPaxFlight.AirlineCode ='FC'
-		and dbo.ACSPaxFlight.OnBrdInd = 'Y'
-		and dbo.ACSPaxFlight.PaxThruInd = 'N'
+--WHERE	dbo.ACSPaxFlight.AirlineCode ='FC'
+--		and dbo.ACSPaxFlight.OnBrdInd = 'Y'
+--		and dbo.ACSPaxFlight.PaxThruInd = 'N'
 
 GROUP BY dbo.ACSPaxFlight.ServiceStartDate, dbo.ACSPaxFlight.FltNbr, dbo.ACSPaxFlight.PaxPNROrigin, dbo.ACSPaxFlight.PaxPNRDest, dbo.ACSPaxFlight.AirlineCode, dbo.ACSPaxFlight.OnBrdInd, dbo.ACSPaxFlight.PaxThruInd
+
+HAVING (((dbo.ACSPaxFlight.AirlineCode) = 'FC') and ((dbo.ACSPaxFlight.OnBrdInd)='Y') AND ((dbo.ACSPaxFlight.PaxThruInd)='N'))
 
 ORDER BY dbo.ACSPaxFlight.ServiceStartDate, dbo.ACSPaxFlight.FltNbr, dbo.ACSPaxFlight.PaxPNROrigin, dbo.ACSPaxFlight.PaxPNRDest;
 GO
